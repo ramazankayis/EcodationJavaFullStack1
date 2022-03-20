934310838a066c4214397e3e13401a4666d65c0f
ramazankayis <kayis.ramazan@gmail.com>
2022-03-20 12:47:48 +0300

    dereceyi fahrenhayta çevirme ödev

--- src/main/resources/static/JavaScriptOdev/template.js
+++ src/main/resources/static/JavaScriptOdev/template.js
@@ -1,6 +1,5 @@
-
 //üslü sayı alan fonksiyon yazalım Math.pow()
-//örnek : 2 üssü 4 = 16 
+//örnek : 2 üssü 4 = 16
 // let usluSayı = (baseNumber,powerOfNumber)=>{
 
 //   if(baseNumber<0)
@@ -10,27 +9,37 @@
 
 //     for (let i = 0; i < powerOfNumber; i++) {
 //       result *= baseNumber;
-      
+
 //     }
-  
-//     return result; 
+
+//     return result;
 // }
 
 // let tabanSayisi= Number(prompt("Lütfen bir sayı giriniz"));
 // let ussuSayisi= Number(prompt("Lütfen bir sayı giriniz"));
 // console.log("üslü sayı = ",usluSayı(tabanSayisi,ussuSayisi));
 
-let tabanSayisi= Number(prompt("Lütfen bir sayı giriniz"));
-let ussuSayisi= Number(prompt("Lütfen bir sayı giriniz"));
+// let tabanSayisi= Number(prompt("Lütfen bir sayı giriniz"));
+// let ussuSayisi= Number(prompt("Lütfen bir sayı giriniz"));
 
-let usluSayı2 = (baseNumber,powerOfNumber)=>{
+// let usluSayı2 = (baseNumber,powerOfNumber)=>{
 
-  let result= Math.pow(baseNumber,powerOfNumber);
-  //console.log("result",result);
+//   let result= Math.pow(baseNumber,powerOfNumber);
+//   //console.log("result",result);
 
-   return result;
-}
+//    return result;
+// }
 
-
-console.log("üslü sayı = ",usluSayı2(tabanSayisi,ussuSayisi));
+// console.log("üslü sayı = ",usluSayı2(tabanSayisi,ussuSayisi));
 //usluSayı2(tabanSayisi,ussuSayisi);
+
+//****************************************************************** */
+//dereceyi fahrenhayta çeviren function
+// let sayi = Number(prompt('Lütfen bir sayı giriniz'));
+// let hesapla = (sayi) => {
+//   let derece = sayi * 1.8 + 32;
+
+//   return derece.toFixed(3);
+// };
+
+// console.log(sayi + ' derece = ' + hesapla(sayi) + " Fahrenheit'dir");
