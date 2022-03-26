c3fd10ce36d310b0ebb308a2926eeb05c39f193e
ramazankayis <kayis.ramazan@gmail.com>
2022-03-26 12:15:17 +0300

    revize

--- src/main/resources/static/jQuery odev/jquery.js
+++ src/main/resources/static/jQuery odev/jquery.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
   $('#has2').click(function () {
     $('li').has('span').css('background-color', 'blue');
   });
+
+  $('#img_id').attr('width', '50%').attr('height', '100px');
 });
 
 $(document).ready;
